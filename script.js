3e59fb6b5d9a632dfba908680928d232d5eaf117
Mohsin Raza Abro <61912563+mohsinrazaabro@users.noreply.github.com>
2021-10-28 15:34:38 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ let night = true;
 let nightBtn = document.getElementById("night-mode");
 let toggleBtn = document.getElementById("toggle-btn");
 let navLinks = document.getElementsByClassName("nav-links")[0];
-
+let white = "rgb(241, 241, 241)";
+let black = "rgb(37, 37, 37)";
 nightBtn.onclick = () => {
   document.documentElement.style.setProperty(
     "--main-bg-color",
-    night ? "black" : "white"
+    night ? black : white
   );
   document.documentElement.style.setProperty(
     "--main-color",
-    night ? "white" : "black"
+    night ? white : black
   );
   night = !night;
 };
